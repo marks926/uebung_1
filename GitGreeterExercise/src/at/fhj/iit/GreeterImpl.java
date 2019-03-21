040097cfbc71084e9c658d0226be179986180bd6
Daniel Marks <daniel.marks@edu.campus02.at>
2019-03-21 13:18:47 +0100

    Klasse GreaterImpl erstellt

---
+++ GitGreeterExercise/src/at/fhj/iit/GreeterImpl.java
@@ -0,0 +1,22 @@
+package at.fhj.iit;
+
+public class GreeterImpl implements Greeter {
+	
+	
+	GreeterImpl(String string){
+		
+	}
+	
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sayGoodbye() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
